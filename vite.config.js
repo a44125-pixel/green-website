88f5537decefd5e4bde89cae58151906bc5ab48a
seven7 <seven7@DESKTOP-Q9PQQHR.localdomain>
2026-04-18 18:39:29 +0100

    Fix: Explicit multi-page config and translate hydro

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        // Ficheiros que já lá estavam
         eolica: resolve(__dirname, 'eolica.html'),
         solar: resolve(__dirname, 'solar.html'),
         hidro: resolve(__dirname, 'hidro.html'),
+        // Seus novos ficheiros de desenho
+        desenho_solar: resolve(__dirname, 'Desenh.html'),
+        desenho_hidro: resolve(__dirname, 'Desenh1.html'),
+        desenho_eolica: resolve(__dirname, 'Desenh2.html'),
       },
     },
   },
